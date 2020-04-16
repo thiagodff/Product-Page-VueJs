ac47f23f5cb969fb85aa75724eff9e90f05b2502
thiagofd <thiago.fdornelles@gmail.com>
2020-04-16 14:15:20 -0300

    attribute binding

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-var product = 'Socks';
+options = {
+  el: '#app',
+  data: {
+    product: 'Socks',
+    image: './assets/vmSocks-green-onWhite.jpg',
+    vuemastery: 'https://www.vuemastery.com/courses/intro-to-vue-js',
+  },
+};
+
+var app = new Vue(options);
