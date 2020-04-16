89dbce892efe54c29247fe29dd237ab70e0f4f7b
thiagofd <thiago.fdornelles@gmail.com>
2020-04-16 14:22:06 -0300

    conditional rendering

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ options = {
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
     vuemastery: 'https://www.vuemastery.com/courses/intro-to-vue-js',
+    onSale: true,
+    inventory: 10,
   },
 };
 
